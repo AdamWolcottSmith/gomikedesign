aa2e44f2e35e52d864ec0bcc4abaf87a63a82575
Adam Wolcott Smith <adamwolcottsmith@gmail.com>
2020-07-07 13:09:35 -0400

    undone php compression

--- index.php
+++ index.php
@@ -1,8 +1,1 @@
 <?php header( 'Location: /index.html' ) ;  ?>
-<?php
-$data = implode("", file("./js/jquery-2.1.0.js"));
-$gzdata = gzencode($data, 9);
-$fp = fopen("./js/jquery-2.1.0.js", "w");
-fwrite($fp, $gzdata);
-fclose($fp);
-?>
