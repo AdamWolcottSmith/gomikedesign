eb30c057bef0abe34d39e6ca24776e51402d1ae5
Adam Wolcott Smith <adamwolcottsmith@gmail.com>
2020-07-07 12:43:03 -0400

    php text compress test

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
 <?php header( 'Location: /index.html' ) ;  ?>
+<?php
+$data = implode("", file("./js/jquery-2.1.0.js"));
+$gzdata = gzencode($data, 9);
+$fp = fopen("./js/jquery-2.1.0.js", "w");
+fwrite($fp, $gzdata);
+fclose($fp);
+?>
